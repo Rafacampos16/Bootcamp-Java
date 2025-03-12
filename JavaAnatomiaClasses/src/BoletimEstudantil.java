94798c42aa7384497d45559d767202408a176c0c
Rafa <108197293+Rafacampos16@users.noreply.github.com>
2025-03-11 23:00:15 -0300

    Classe05

--- JavaAnatomiaClasses/src/BoletimEstudantil.java
+++ JavaAnatomiaClasses/src/BoletimEstudantil.java
@@ -1,7 +1,13 @@
-
-public class MinhaClasse {
+public class BoletimEstudantil {
 
     public static void main(String[] args) {
-        System.out.println("Oieeee");
+        int mediaFinal = 6;
+        if (mediaFinal < 6) {
+            System.out.println("REPROVADO"); 
+        }else if (mediaFinal == 6) {
+            System.out.println("PROVA MINERVA"); 
+        }else {
+            System.out.println("APROVADO");
+        }
     }
 }
